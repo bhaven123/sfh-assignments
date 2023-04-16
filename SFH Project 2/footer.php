6eae627f3197f9ffc8d041bb34698761ebcada8b
bhaven123 <naikbhaven11@gmail.com>
2023-04-16 11:36:44 -0300

    Initial commit

---
+++ SFH Project 2/footer.php
@@ -0,0 +1,11 @@
+<!-- Importing Bootstrap and CSS files -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/main.css" />
+<footer>
+    <nav>
+        <ul>
+            <li>&copy; 2023 Created for SFH Project 2</li>
+        </ul>
+    </nav>
+</footer>
+
